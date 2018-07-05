9207280fcc96b100314bb94ef812f134907d7662
Tolotra Raharison <tolotra.raharison@shaft-it.com>
2018-07-05 17:56:53 +0200

    fix demandes

--- src/components/demandes/demandes.container.js
+++ src/components/demandes/demandes.container.js
@@ -1,26 +1,39 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { View, Text } from 'react-native'
-import { fetchDemandes } from "../../store/demandes/demandes.actions";
+import { fetchDemandes } from "../../store/demandes/demandes.actions"
+import Loader from "../loader/Loader";
 
 class Demandes extends React.Component {
 
+  /**
+   * Fetch items when component is mounted
+   */
   componentDidMount() {
     this.props.fetchDemandes('')
   }
 
+  /**
+   * Render component
+   * @returns {*}
+   */
   render() {
-    const { items } = this.props
+    const { items, loading } = this.props
+
+    if (loading) {
+      return <Loader loading={true}/>
+    }
+
     return (
       <View>
-        {items.map((item, key) => <Text key={key}>{JSON.stringify(item)}</Text>)}
+        {items && items.map((item, key) => <Text key={key}>{JSON.stringify(item)}</Text>)}
       </View>
     );
   }
 
 }
 
-const mapDispatchToProps = function (dispatch) {
+const mapDispatchToProps = (dispatch) => {
   return {
     fetchDemandes: function (token) {
       dispatch(fetchDemandes(token))
@@ -28,7 +41,7 @@ const mapDispatchToProps = function (dispatch) {
   }
 }
 
-const mapStateToProps = function (state) {
+const mapStateToProps = (state) => {
   return {
     items: state.demandes.items || [],
     loading: state.demandes.loading
