877a4325564533a5eba3fadb2ce5a45c17898210
Tolotra Raharison <tolotra.raharison@shaft-it.com>
2018-07-05 15:19:34 +0200

    add `demandes` to sidebar

--- src/components/appdrawer/config/navigation.js
+++ src/components/appdrawer/config/navigation.js
@@ -1,69 +1,72 @@
 import React from "react";
-import {createStackNavigator, createDrawerNavigator} from 'react-navigation';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
 import DashboardContainer from '../../dashboard/dashboard.container';
 import UserListContainer from '../../usermanager/userlist/userlist.container';
 import MessagesListContainer from '../../messages/list/message.container';
+import DemandesContainer from '../../demandes/demandes.container'
 import DrawerContent from "../sidebar/Sidebar";
-import { Image} from 'react-native';
+import { Image } from 'react-native';
 import { AppHeader } from '../../appheader/appheader.component';
 
 const DrawerNavigator = createDrawerNavigator({
-        Home:{
-            screen: DashboardContainer
-        },
-        User:{
-            screen: UserListContainer
-        },
-        Message:{
-            screen: MessagesListContainer
-        }
-
+    Home: {
+      screen: DashboardContainer
     },
-    {
-        initialRouteName: 'Home',
-        contentComponent: DrawerContent,
-        drawerWidth: 300,
-        drawerPosition: 'right',
+    User: {
+      screen: UserListContainer
+    },
+    Message: {
+      screen: MessagesListContainer
+    },
+    Demandes: {
+      screen: DemandesContainer
     }
+  },
+  {
+    initialRouteName: 'Home',
+    contentComponent: DrawerContent,
+    drawerWidth: 300,
+    drawerPosition: 'right',
+  }
 );
 
-const MenuImage = ({navigation}) => {
-    if(!navigation.state.isDrawerOpen){
-        return <Image source={require('../../../assets/images/menu-button.png')}/>
-    }else{
-        return <Image source={require('../../../assets/images/left-arrow.png')}/>
-    }
+const MenuImage = ({ navigation }) => {
+  if (!navigation.state.isDrawerOpen) {
+    return <Image source={require('../../../assets/images/menu-button.png')}/>
+  } else {
+    return <Image source={require('../../../assets/images/left-arrow.png')}/>
+  }
 }
 
 const StackNavigator = createStackNavigator({
-    
-    //important: key and screen name (i.e. DrawerNavigator) should be same while using the drawer navigator inside stack navigator.
-    
-    DrawerNavigator:{
-        screen: DrawerNavigator
+
+  //important: key and screen name (i.e. DrawerNavigator) should be same while using the drawer navigator inside stack navigator.
+
+  DrawerNavigator: {
+    screen: DrawerNavigator
+  },
+}, {
+  navigationOptions: ({ navigation }) => ({
+
+    //custo header try
+    header: (/* Your custom header */
+      <AppHeader navigation={navigation}/>
+    ),
+
+    /*title: 'ReactNavigation',  // Title to appear in status bar
+    headerLeft:
+    <TouchableOpacity  onPress={() => {navigation.dispatch(DrawerActions.toggleDrawer())} }>
+        <MenuImage style="styles.bar" navigation={navigation}/>
+    </TouchableOpacity>,
+    headerStyle: {
+        backgroundColor: '#333',
     },
-},{
-    navigationOptions: ({ navigation }) => ({
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },*/
 
-        //custo header try
-        header: ( /* Your custom header */
-            <AppHeader navigation={navigation}/>
-        ),
-
-        /*title: 'ReactNavigation',  // Title to appear in status bar
-        headerLeft: 
-        <TouchableOpacity  onPress={() => {navigation.dispatch(DrawerActions.toggleDrawer())} }>
-            <MenuImage style="styles.bar" navigation={navigation}/>
-        </TouchableOpacity>,
-        headerStyle: {
-            backgroundColor: '#333',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },*/
-
-    })
+  })
 });
 
 export default StackNavigator;
--- src/components/appdrawer/sidebar/Sidebar.js
+++ src/components/appdrawer/sidebar/Sidebar.js
@@ -17,51 +17,51 @@ class DrawerContent extends Component {
     this.props.navigation.dispatch(DrawerActions.closeDrawer())
   }
 
-  render () {
-      const { token } = this.props
-        return (
-            <View style={styles.container}>
-                <ScrollView>
-                    <TouchableOpacity style={styles.containerItem} onPress={this.navigateToScreen('Home')}>
-                        <Icon name="dashboard" style={styles.iconItemLeft}/>
-                        <Text style={styles.textItemInside}>Tableau de bord</Text>
-                    </TouchableOpacity>
+  render() {
+    const { token } = this.props
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+          <TouchableOpacity style={styles.containerItem} onPress={this.navigateToScreen('Home')}>
+            <Icon name="dashboard" style={styles.iconItemLeft}/>
+            <Text style={styles.textItemInside}>Tableau de bord</Text>
+          </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.containerItem} onPress={this.navigateToScreen('Home')}>
-                        <Icon name="bar-chart" style={styles.iconItemLeft}/>
-                        <Text style={styles.textItemInside}>Statistiques de campagne</Text>
-                    </TouchableOpacity>
+          <TouchableOpacity style={styles.containerItem} onPress={this.navigateToScreen('Home')}>
+            <Icon name="bar-chart" style={styles.iconItemLeft}/>
+            <Text style={styles.textItemInside}>Statistiques de campagne</Text>
+          </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.containerItem}>
-                        <Icon name="phone" style={styles.iconItemLeft}/>
-                        <Text style={styles.textItemInside}>Liste des appels</Text>
-                    </TouchableOpacity>
+          <TouchableOpacity style={styles.containerItem}>
+            <Icon name="phone" style={styles.iconItemLeft}/>
+            <Text style={styles.textItemInside}>Liste des appels</Text>
+          </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.containerItem} onPress={this.navigateToScreen('Message')}>
-                        <Icon name="envelope" style={styles.iconItemLeft}/>
-                        <Text style={styles.textItemInside}>Liste des messages</Text>
-                    </TouchableOpacity>
+          <TouchableOpacity style={styles.containerItem} onPress={this.navigateToScreen('Message')}>
+            <Icon name="envelope" style={styles.iconItemLeft}/>
+            <Text style={styles.textItemInside}>Liste des messages</Text>
+          </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.containerItem} onPress={this.navigateToScreen('User')}>
-                        <Icon name="users" style={styles.iconItemLeft}/>
-                        <Text style={styles.textItemInside}>Gestion des utilisateurs</Text>
-                    </TouchableOpacity>
+          <TouchableOpacity style={styles.containerItem} onPress={this.navigateToScreen('User')}>
+            <Icon name="users" style={styles.iconItemLeft}/>
+            <Text style={styles.textItemInside}>Gestion des utilisateurs</Text>
+          </TouchableOpacity>
 
-                    <TouchableOpacity style={styles.containerItem}>
-                        <Icon name="ticket" style={styles.iconItemLeft}/>
-                        <Text style={styles.textItemInside}>Gestion des demandes</Text>
-                    </TouchableOpacity>
+          <TouchableOpacity style={styles.containerItem} onPress={this.navigateToScreen('Demandes')}>
+            <Icon name="ticket" style={styles.iconItemLeft}/>
+            <Text style={styles.textItemInside}>Gestion des demandes</Text>
+          </TouchableOpacity>
 
-                    <View style={styles.containerItemDevider}/>
+          <View style={styles.containerItemDevider}/>
 
-                    <TouchableOpacity style={styles.containerItem} onPress={() => this.props.logout(token)}>
-                        <Icon name="power-off" style={styles.iconItemLeft}/>
-                        <Text style={styles.textItemInside}>Deconnexion</Text>
-                    </TouchableOpacity>
-                </ScrollView>
-            </View>
-        );
-    }
+          <TouchableOpacity style={styles.containerItem} onPress={() => this.props.logout(token)}>
+            <Icon name="power-off" style={styles.iconItemLeft}/>
+            <Text style={styles.textItemInside}>Deconnexion</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 DrawerContent.propTypes = {
@@ -69,11 +69,11 @@ DrawerContent.propTypes = {
 };
 
 const mapDispatchToProps = dispatch => ({
-    logout: (token) => dispatch(logout(token))
+  logout: (token) => dispatch(logout(token))
 })
 
 const mapStateToProps = state => ({
-    token: state.login.item.mobile_token
+  token: state.login.item.mobile_token
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(DrawerContent)
