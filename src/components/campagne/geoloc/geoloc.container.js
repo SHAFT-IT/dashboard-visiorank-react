90d7381b475732cfdefc91bcea904c99d6460d6a
Tahiry Randriambola <tahiry.randriambola@shaft-it.com>
2018-07-11 11:12:24 +0300

    ignore

--- src/components/campagne/geoloc/geoloc.container.js
+++ src/components/campagne/geoloc/geoloc.container.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {View, Text} from 'react-native'
+import {View, StyleSheet} from 'react-native'
 
 export default class GeolocContainer extends Component{
 
@@ -8,13 +8,24 @@ export default class GeolocContainer extends Component{
     }
 
     render() {
+      const { region } = this.props;
+      console.log(region);
         return (
-          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff'}}>
-            <Text style={{fontSize: 20, textAlign: 'center', margin: 10}}>
-              Geolocalisation
-            </Text>
-          </View>
+          <View style ={styles.container}>
+          Geolocalisation
+        </View>
         );
     }
-
 }
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
