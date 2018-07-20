9a011edc9155d15d7c8bb309d147d1739df03467
randrianarivo-mandimby <mandimby.randrianarivo@shaft-it.com>
2018-07-20 16:07:19 +0200

    make tab campagne in bottom

---
+++ src/components/demandes/attachment/attachment.container.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import {View, Text} from 'react-native'
+
+export default class AttachmentContainer extends Component{
+
+    render() {
+
+        return (
+
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+            
+                <Text>PIECE JOINTE</Text>
+            
+            </View>
+
+        );
+
+    }
+
+
+}
