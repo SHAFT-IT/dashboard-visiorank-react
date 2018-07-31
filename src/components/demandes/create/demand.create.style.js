df1dbc072265cd310e7edd4c421675f13a36a8f1
randrianarivo-mandimby <mandimby.randrianarivo@shaft-it.com>
2018-07-31 12:41:30 +0200

    separate style to component in demande create

--- src/components/demandes/create/demand.create.style.js
+++ src/components/demandes/create/demand.create.style.js
@@ -1,11 +1,31 @@
  export default {
+     
+    richText: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'transparent',
+    },
     allcontent: {
         flex: 1,
-        flexDirection: 'column'
     },
     container: {
         backgroundColor: '#F5FCFF',
         paddingTop: 25
+    },
+    bsubmit1: {
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 1,
+        marginBottom: 5,
+        height: 50,
+        borderRadius: 3,
+        width: 300,
+        padding: 5,
+        marginLeft: 30,
+        backgroundColor: '#5bc0de',
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: 'center'
     },
     autocompleteContainer: {
         left: 0,
@@ -16,7 +36,7 @@
         marginTop: 20,
         borderRadius: 6,
         borderWidth: 0.5,
-        position:'relative',
+        position: 'relative',
         borderColor: '#939393',
         zIndex: 1
     },
@@ -34,6 +54,13 @@
         backgroundColor: '#F5FCFF',
         marginTop: 25
     },
+    selectedButtonStyle: {
+        backgroundColor: 'green',
+    },
+    selectedTextStyle: {
+        color: 'white',
+    },
+
     infoText: {
         textAlign: 'center'
     },
@@ -57,10 +84,44 @@
         marginLeft: 30,
         marginRight: 30,
         marginTop: 20,
-        fontSize: 14,
-        fontWeight: 'bold',
+        fontSize: 10,
+        //fontWeight: 'bold',
         textAlign: 'left',
         height: 45,
+        borderRadius: 6,
+        borderWidth: 1,
+        paddingLeft: 15,
+        borderColor: '#939393',
+    },
+
+    buttonGroupTitle: {
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 20,
+        fontSize: 10,
+        fontWeight: 'bold',
+        textAlign: 'left'
+    },
+    buttonGroupBackground: {},
+    buttonGroupContainer: {
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 10,
+    },
+    buttonGroup: {
+        fontSize: 10,
+        //fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    textArea: {
+        textAlignVertical: 'top',
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 20,
+        fontSize: 10,
+        //fontWeight: 'bold',
+        textAlign: 'left',
+        height: 150,
         borderRadius: 6,
         borderWidth: 1,
         paddingLeft: 15,
@@ -70,13 +131,29 @@
         marginTop: 17,
     },
     icontop: {
-        fontSize: 45,
+        fontSize: 35,
         color: 'grey',
+    },
+    iconright: {
+        fontSize: 35,
+        color: 'grey',
+    },
+    icontopStatus: {
+        fontSize: 10,
     },
     containericontop: {
         position: 'absolute',
         top: 10,
         left: 25,
+        alignItems: "center",
+        justifyContent: "center",
+        width: 50,
+        height: 50,
+    },
+    containericonright: {
+        position: 'absolute',
+        top: 10,
+        right: 25,
         alignItems: "center",
         justifyContent: "center",
         width: 50,
@@ -96,19 +173,26 @@
     },
     buttonText: {
         color: 'white',
+        fontSize: 12,
+        //fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    buttonSubmitText: {
+        color: 'white',
         fontSize: 16,
         fontWeight: 'bold',
         textAlign: 'center',
     },
     bigtitle: {
-        textAlign: 'center',
+        textAlign: 'left',
         color: '#939393',
-        fontSize: 18,
+        fontSize: 14,
         fontWeight: 'bold',
         marginBottom: 20,
-        marginTop: 20
+        marginLeft: 70,
+        marginTop: 25
     }
-}
+};
 
 
 
