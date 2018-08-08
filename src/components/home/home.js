b52a489ca195a8cde6ac8e73df43750d2321d3cb
Tolotra Raharison <tolotra.raharison@shaft-it.com>
2018-08-08 13:09:39 +0200

    fix login

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
-import Drawer from "../appdrawer/config/navigation";
+import Drawer from '../appdrawer/config/navigation';
 import LoginContainer from '../login/login.container';
-import AuthLoadingScreen from "../authloading/AuthLoadingScreen";
+import AuthLoadingScreen from '../authloading/AuthLoadingScreen';
 
 const HomeSwitchNavigator = createSwitchNavigator(
-    {
-        AuthLoading: AuthLoadingScreen,
-        Authentification: LoginContainer,
-        Home: Drawer,
-    },
-    {
-        initialRouteName: 'AuthLoading',
-    }
+  {
+    AuthLoading: AuthLoadingScreen,
+    Authentification: LoginContainer,
+    Home: Drawer
+  },
+  {
+    initialRouteName: 'AuthLoading'
+  }
 );
 
 export default HomeSwitchNavigator;
