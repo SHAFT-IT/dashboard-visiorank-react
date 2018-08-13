cd8e6cd6e1da51b3159f19c0a33b77ee0d9319e5
Tolotra Raharison <tolotra.raharison@shaft-it.com>
2018-08-13 09:55:08 +0200

    fix map and chart 'replace onSelect to onChartSelect' from this issue https://github.com/react-community/react-native-maps/issues/1615

--- App.js
+++ App.js
@@ -4,18 +4,18 @@
  * @flow
  */
 
-import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
-import { Provider } from "react-redux";
-import OfflineNotice from "./src/components/internet/offline.notice";
-import Splash from "./src/components/splash/splash";
-import store from "./src/store";
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Provider } from 'react-redux';
+import OfflineNotice from './src/components/internet/offline.notice';
+import Splash from './src/components/splash/splash';
+import store from './src/store';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      prefuser: "",
+      prefuser: ''
     };
   }
 
@@ -27,8 +27,8 @@ export default class App extends Component {
     return (
       <Provider store={store()}>
         <View style={styles.container}>
-          <OfflineNotice />
-          <Splash />
+          <OfflineNotice/>
+          <Splash/>
         </View>
       </Provider>
     );
@@ -37,6 +37,6 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
+    flex: 1
+  }
 });
