397674d902ea343bca8dd0d9a2b79baf6790cb5f
Tolotra Raharison <tolotra.raharison@shaft-it.com>
2018-08-17 09:52:04 +0200

    fix appel layout on iPhone

--- src/components/appel/appel.component.js
+++ src/components/appel/appel.component.js
@@ -1,62 +1,65 @@
-import React from 'react'
-import { Text, View, ListView, Switch } from 'react-native';
+import React from 'react';
+import { ListView, Switch, Text, View } from 'react-native';
 import styles from './appel.style';
-import { connect } from 'react-redux'; 
+import { connect } from 'react-redux';
 import { setPertinance } from '../../store/actions/appel.action';
 import moment from 'moment';
-import 'moment/locale/fr'
+import 'moment/locale/fr';
+
 moment.locale('fr');
 
 class Appels extends React.Component {
-    constructor(props) {
-        super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = { dataSource: ds.cloneWithRows(props.appels) };
-    }
-    render() {
-        const { currentItem } = this.props
-        return ( 
-            <ListView 
-                dataSource={this.state.dataSource} 
-                renderRow = {item => <AppelItem item={item} dispatch={this.props.dispatch} currentItem={currentItem} />} 
-            /> 
-        );
-    }
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = { dataSource: ds.cloneWithRows(props.appels) };
+  }
+
+  render() {
+    const { currentItem } = this.props;
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={item => <AppelItem item={item} dispatch={this.props.dispatch} currentItem={currentItem}/>}
+      />
+    );
+  }
 }
 
-class  AppelItem extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = { 
-            switchOnValueHolder :parseInt(props.item.pertinant) === 1 
-        };
-    }
+class AppelItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchOnValueHolder: parseInt(props.item.pertinant) === 1
+    };
+  }
 
-    savePertinance = (value, id) => {
-        this.setState({ 
-            switchOnValueHolder: value 
-        })
-        this.props.dispatch (setPertinance(id))
-    }
+  savePertinance = (value, id) => {
+    this.setState({
+      switchOnValueHolder: value
+    });
+    this.props.dispatch(setPertinance(id));
+  };
 
-    render() {
-        const { item } = this.props
-        const dStart = moment(item.starttime).format('Do MMM YY')
-        const hStart = moment(item.starttime).format('hh:mm')
+  render() {
+    const { item } = this.props;
+    const dStart = moment(item.starttime).format('Do MMM YY');
+    const hStart = moment(item.starttime).format('hh:mm');
 
-        return (
-            <View style={styles.item}>
-                <View style={styles.itemLeft}>
-                    <Text style={ styles.itemtextdate }>{dStart} à {hStart}</Text> 
-                    <Text style={ styles.itemtext }>{item.duration}</Text> 
-                    <Text style={styles.itemtext }>{ item.source }</Text> 
-                </View>
-                <View style={styles.itemRight}>
-                    <Switch value={item.pertinant}  onValueChange={(value) => this.savePertinance(value, item.id)} style={{marginBottom: 10}} value={this.state.switchOnValueHolder} />
-                </View>
-            </View>
-        )
-    }
+    return (
+      <View style={styles.item}>
+        <View style={styles.itemLeft}>
+          <Text style={styles.itemtextdate}>{dStart} à {hStart}</Text>
+          <Text style={styles.itemtext}>{item.duration}</Text>
+          <Text style={styles.itemtext}>{item.source}</Text>
+        </View>
+        <View style={styles.itemRight}>
+          <Switch value={item.pertinant} onValueChange={(value) => this.savePertinance(value, item.id)}
+                  style={{ marginBottom: 10 }} value={this.state.switchOnValueHolder}/>
+        </View>
+      </View>
+    );
+  }
 }
 
-export default connect()(Appels)
+export default connect()(Appels);
--- src/components/appel/appel.style.js
+++ src/components/appel/appel.style.js
@@ -1,72 +1,71 @@
-export default  {
+export default {
 
-    container: {
-        backgroundColor: 'white',
-        flex: 1,
-        flexDirection: 'column'
-    },
-    item: {
-        flex: 1, 
-        flexDirection: 'row',
-        marginLeft: 0,
-        marginRight: 0,
-        marginTop: 1,
-        paddingVertical : 10,
-        backgroundColor: '#DCDCDC',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    itemLeft: {
-        flex: 1, 
-        flexDirection: 'column',
-        marginLeft: 0,
-        marginRight: 0,
-        marginTop: 1,
-        backgroundColor: '#DCDCDC',
-        justifyContent: 'center',
-    },
-    itemRight: {
-        flex: 1, 
-        flexDirection: 'column',
-        marginLeft: 0,
-        marginRight: 10,
-        marginTop: 1,
-        width: 150,
-        backgroundColor: '#DCDCDC',
-        justifyContent: 'center',
-    },
-    itemtext: {
-        marginLeft: 20,
-        marginRight: 20,
-        fontSize: 16,
-        textAlign: 'left',
-        color: 'black'
-    },
-    itemtextunseen: {
-        marginLeft: 20,
-        marginRight: 20,
-        fontSize: 16,
-        textAlign: 'left',
-        color: 'black',
-        fontWeight: 'bold'
-    },
-    itemtextdate: {
-        marginLeft: 20,
-        marginRight: 20,
-        fontSize: 12,
-        textAlign: 'left',
-        color: '#7d7b7b'
-    },
-    errortext: {
-        flex: 1,
-        color: 'black',
-        textAlign: "center",
-        textAlignVertical: "center",
-        fontSize: 15,
-    },
-    iconleft: {
-        position: 'absolute',
-        fontSize: 40,
-        color: 'grey'
-    },
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    flexDirection: 'column'
+  },
+  item: {
+    flex: 1,
+    flexDirection: 'row',
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 1,
+    paddingVertical: 10,
+    backgroundColor: '#DCDCDC',
+    justifyContent: 'space-between',
+    alignItems: 'space-between'
+  },
+  itemLeft: {
+    flex: 1,
+    flexDirection: 'column',
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 1,
+    backgroundColor: '#DCDCDC',
+    justifyContent: 'center'
+  },
+  itemRight: {
+    flexDirection: 'column',
+    marginLeft: 0,
+    marginRight: 10,
+    marginTop: 1,
+    width: 50,
+    backgroundColor: '#DCDCDC',
+    justifyContent: 'center'
+  },
+  itemtext: {
+    marginLeft: 20,
+    marginRight: 20,
+    fontSize: 16,
+    textAlign: 'left',
+    color: 'black'
+  },
+  itemtextunseen: {
+    marginLeft: 20,
+    marginRight: 20,
+    fontSize: 16,
+    textAlign: 'left',
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  itemtextdate: {
+    marginLeft: 20,
+    marginRight: 20,
+    fontSize: 12,
+    textAlign: 'left',
+    color: '#7d7b7b'
+  },
+  errortext: {
+    flex: 1,
+    color: 'black',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 15
+  },
+  iconleft: {
+    position: 'absolute',
+    fontSize: 40,
+    color: 'grey'
+  }
 };
